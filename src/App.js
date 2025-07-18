9ff639d59d7b6f18759386d661235ee54d957189
Xrzchs <121417826+Xrzchs@users.noreply.github.com>
2025-07-18 04:15:49 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Twilio Web Calling App</h1>
+      <p>Frontend is running!</p>
+    </div>
+  );
+}
+export default App;
